bc5dd21946cfb8529d0a94a2470c2420b511a15d
Kenny Lov <lov.kenny1996@gmail.com>
2018-08-03 15:09:41 -0700

    fixed js

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,26 @@
+var shrunk = false;
 $(window).scroll(function() {
   if ($(document).scrollTop() > 150) {
     $('nav').addClass('shrink');
+    shrunk = true;
   } else {
     $('nav').removeClass('shrink');
+	$('nav').removeClass('hovered');
+    shrunk = false;
   }
 });
+
+nav = $('nav');
+
+function onHover(){
+  if(shrunk===true){
+    nav.addClass('hovered');
+  }
+}
+
+function noHover(){
+  nav.removeClass('hovered');
+}
+
+nav[0].addEventListener("mouseover", onHover);
+nav[0].addEventListener("mouseout", noHover);
