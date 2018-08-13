64ba60b8b440a4adff37d772380933aa0e711e8f
Kenny Lov <lov.kenny1996@gmail.com>
2018-08-12 17:56:04 -0700

    editing js

--- js/projects.js
+++ js/projects.js
@@ -23,3 +23,21 @@ $(window).scroll(function() {
 
 
 });
+
+
+
+var page_y = $( document ).scrollTop();
+window.location.href = window.location.href + '?page_y=' + page_y;
+
+
+//code to handle setting page offset on load
+$(function() {
+    if ( window.location.href.indexOf( 'page_y' ) != -1 ) {
+        //gets the number from end of url
+        var match = window.location.href.split('?')[1].match( /\d+$/ );
+        var page_y = match[0];
+
+        //sets the page offset 
+        $( 'html, body' ).scrollTop( page_y );
+    }
+});
